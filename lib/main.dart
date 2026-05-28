ac92d98ff2498c960abcb645aa485d3de37ae958
Mhmoud-Hota <atbarahota@gmail.com>
2026-05-28 14:04:57 +0200

    Remove unnecessary blank line in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'core/di/service_locator.dart' as di;
 import 'config/routes/app_router.dart';
 import 'features/welcome/presentation/cubit/welcome_cubit.dart';
